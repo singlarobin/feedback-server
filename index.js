3a6219db421247d2788e96bf13acf4e156d63a36
singlarobin <singlarobin99@gmail.com>
2021-11-08 23:05:43 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({Hi: 'there'})
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log("Server is Listening!!"));
